af4ecfc2bafa12175185bb3c3267aaca4935b4a3
Seito Yokota <seitoyokota15@icloud.com>
2026-03-19 14:42:29 -0500

    loop

---
+++ loops.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(){
+  int i, j, k, rows;
+
+  for(i=0;i<rows;i++){
+    for(k=0;k<=(i*2);k++){
+      printf(" ");
+    }
+    for(j=0;j<=4-i;j++){
+      printf("*");
+    }
+    printf("\n");
+  }
+  for(i=0;i<rows;i++){
+    for(k=0;k<=(i*2);k++){
+      printf(" ");
+    }
+    for(j=0;j<=4-i;j++){
+      printf("*");
+    }
+    printf("\n");
+  }
+  for(i=0;i<rows;i++){
+    
+  }
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
