ab617458e2abac5c5819a5ce648db8e191dfb7e1
Seito Yokota <seitoyokota15@icloud.com>
2026-03-19 13:09:59 -0500

    drawing

---
+++ drawing.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+int main(){
+
+  //This is for output
+  printf("Seito Yokota\n");
+  printf("*************************\n");
+  printf("************************\n");
+  printf("***********************\n");
+  printf("**********************\n");
+  printf("*********************\n");
+  printf("********************\n");
+  printf("*******************\n");
+  printf("******************\n");
+  printf("*****************\n");
+  printf("****************\n");
+  printf("***************\n");
+  printf("**************\n");
+  printf("*************\n");
+  printf("************\n");
+  printf("***********\n");
+  printf("**********\n");
+  printf("*********\n");
+  printf("********\n");
+  printf("*******\n");
+  printf("******\n");
+  printf("*****\n");
+  printf("****\n");
+  printf("***\n");
+  printf("**\n");
+  printf("*\n");
+
+
+  printf("\n\nThis is my drawing\n");
+  printf("^^^^^^^^^^^^^^^^^^^^            ______              ______                                          _____\n");
+  printf("l                  l            l         l         l    l     l\\   l          l\\     /l   l    l   l       l /\n");
+  printf("l                  l            l---      l         l    l     l \\  l          l \\    /l   l    l   -----   l/\n");
+  printf("l     $     $      l            l         l         l    l     l  \\ l          l  \\  / l   l    l       l   l\\\n");
+  printf("l                  l            ------    l-----    ------     l   \\l          l   \\/  l   ------   -----   l \\\n");
+  printf("l                  l            _________________________________\n");
+  printf("l    _________     l           l                                l\n");
+  printf("l                  l           l                       ______   l\n");
+  printf("--------------------           l       _______         l    l   l\n");
+  printf("         /l\\                   l          l            l    l   l\n");
+  printf("        / l \\                  l          l            ------   l\n");
+  printf("       /  l  \\           ______l          l                     l______\n");
+  printf("      /   l   \\-------  l                                             l\n");
+  printf("          l    l  $  l  l                                             l\n");
+  printf("         / \\   -------  l                                             l\n");
+  printf("        /   \\           _______________________________________________\n");
+  printf("       /     \\               -----                              -----\n");
+  printf("      /       \\             l    l                             l    l\n");
+  printf("      l        l            l    l                             l    l\n");
+  printf("     --        --            -----                              -----\n");
+  
+  return 0;
+}
